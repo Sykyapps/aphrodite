03f1a132037364c694d45a6492afdbd80faa1d91
Irfan Nurghiffari Muhajir <148177230+irfanatsayakaya@users.noreply.github.com>
2024-03-27 14:09:58 +0700

    feat: add arrow left icon

---
+++ src/components/icons/arrowLeftIcon.tsx
@@ -0,0 +1,17 @@
+import type { SVGProps } from "react"
+const SvgArrowLeftIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="#1C1D1F"
+      d="M9.636 12 16 18.223 14.182 20 6 12l8.182-8L16 5.777z"
+    />
+  </svg>
+)
+export default SvgArrowLeftIcon
