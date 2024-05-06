383048f7cd6ff2fd4c9b03b6f1514594448b95b1
Irfan Nurghiffari Muhajir <148177230+irfanatsayakaya@users.noreply.github.com>
2024-05-06 18:56:06 +0700

    feat: add new icons

---
+++ src/components/icons/editIcon.tsx
@@ -0,0 +1,17 @@
+import type { SVGProps } from "react"
+const SvgEditIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 16 16"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="m8.543 4.94 2.518 2.518-5.877 5.876H2.667v-2.519L8.543 4.94m.839-.84 1.259-1.26a.594.594 0 0 1 .84 0l1.678 1.68a.593.593 0 0 1 0 .84L11.9 6.617z"
+    />
+  </svg>
+)
+export default SvgEditIcon
---
+++ src/components/icons/sendIcon.tsx
@@ -0,0 +1,17 @@
+import type { SVGProps } from "react"
+const SvgSendIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 13 16"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M0 8.667h4V7.333H0V1.231A.333.333 0 0 1 .494.939l12.308 6.77a.333.333 0 0 1 0 .583L.494 15.062A.333.333 0 0 1 0 14.768z"
+    />
+  </svg>
+)
+export default SvgSendIcon
