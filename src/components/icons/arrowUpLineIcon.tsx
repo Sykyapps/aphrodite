2529b758b7bd2de8556e1cd2c85476f1c3f32c36
Irfan Nurghiffari Muhajir <148177230+irfanatsayakaya@users.noreply.github.com>
2024-06-02 21:51:58 +0700

    feat: add new icons

---
+++ src/components/icons/arrowUpLineIcon.tsx
@@ -0,0 +1,24 @@
+import type { SVGProps } from "react"
+const SvgArrowUpLineIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <g clipPath="url(#arrowUpLineIcon_svg__a)">
+      <path
+        fill="currentColor"
+        d="M13 7.828V20h-2V7.828l-5.364 5.364-1.414-1.414L12 4l7.778 7.778-1.414 1.414z"
+      />
+    </g>
+    <defs>
+      <clipPath id="arrowUpLineIcon_svg__a">
+        <path fill="#fff" d="M0 0h24v24H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+)
+export default SvgArrowUpLineIcon
