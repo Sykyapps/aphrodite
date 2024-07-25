19f8a5ada66add2a781129ae539beb33f596484c
Irfan Nurghiffari Muhajir <148177230+irfanatsayakaya@users.noreply.github.com>
2024-07-25 10:52:37 +0700

    feat: add upload

---
+++ src/components/upload/upload.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import Button from "../button"
+import { Form, FormItem, FormItemContent } from "../forms"
+
+import { default as Upload } from "./upload"
+
+const meta = {
+  title: "Forms/Upload",
+  component: Upload,
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component: "A Upload is input for user to select time.",
+      },
+    },
+  },
+} satisfies Meta<typeof Upload>
+
+type Story = StoryObj<typeof Upload>
+
+export const Default: Story = {
+  render: () => {
+    return <Upload title="Hello" />
+  },
+}
+
+export const WithForm: Story = {
+  render: () => {
+    const [form] = Form.useForm()
+
+    const handleOnFinish = (values: any) => {
+      console.log(values)
+    }
+
+    return (
+      <Form
+        form={form}
+        layout="vertical"
+        requiredMark={false}
+        onFinish={handleOnFinish}
+      >
+        <FormItem
+          name="email"
+          label="Email"
+          getValueFromEvent={(e) => console.log(e)}
+          item={({ name, onChange }: FormItemContent) => (
+            <Upload name={name} onChange={onChange} multiple />
+          )}
+        />
+        <Button htmlType="submit">Submit</Button>
+      </Form>
+    )
+  },
+}
+
+export default meta
---
+++ src/components/upload/upload.tsx
@@ -0,0 +1,103 @@
+import { useEffect, useState } from "react"
+import { Upload as BaseUpload, UploadFile } from "antd"
+
+import Button from "../button"
+import { CloseIcon } from "../icons"
+
+type UploadProps = {
+  name?: string
+  title?: string
+  placeholder?: string
+  multiple?: boolean
+  onChange?: (file: any) => void
+  value?: any
+}
+
+const Upload = ({
+  title = "Upload",
+  placeholder = "No file choosen",
+  multiple = false,
+  onChange,
+  value,
+  ...props
+}: UploadProps) => {
+  const [fileList, setFileList] = useState<UploadFile[]>([])
+
+  const handleOnRemove = (file: any) => {
+    if (!multiple) {
+      setFileList([])
+      onChange && onChange([])
+      return
+    }
+
+    const index = fileList.indexOf(file)
+    const newFileList = fileList.slice()
+    newFileList.splice(index, 1)
+    setFileList(newFileList)
+    onChange && onChange(newFileList)
+  }
+
+  const handleOnUpload = (e: any) => {
+    const file = e.file
+
+    if (multiple) {
+      setFileList([...fileList, file])
+      onChange && onChange([...fileList, file])
+      return
+    }
+
+    setFileList([file])
+    onChange && onChange([file])
+  }
+
+  useEffect(() => {
+    value = fileList
+  }, [fileList])
+
+  return (
+    <div className="flex gap-4">
+      <div className="w-[307px]">
+        {fileList.length > 0 ? (
+          <div className="flex flex-col gap-2">
+            {fileList.map((file) => (
+              <div
+                key={file.uid}
+                className="flex justify-between items-center gap-2 p-2 bg-neutral-100 rounded w-full"
+              >
+                <a
+                  href={file.url}
+                  target="_blank"
+                  className="text-active-text hover:text-purple-300 line-clamp-1"
+                >
+                  {file.name}
+                </a>
+                <Button
+                  className="!h-auto text-lowEmphasis-iconPrimary"
+                  buttonVariant="icon"
+                  onClick={() => handleOnRemove(file)}
+                >
+                  <CloseIcon />
+                </Button>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="flex space-between items-center gap-2 p-2 bg-neutral-100 rounded">
+            <span className="text-[#989cac]">{placeholder}</span>
+          </div>
+        )}
+      </div>
+      <BaseUpload
+        name={props.name}
+        onChange={handleOnUpload}
+        maxCount={1}
+        fileList={[]}
+      >
+        <Button buttonVariant="primary">{title}</Button>
+      </BaseUpload>
+    </div>
+  )
+}
+
+export type { UploadProps }
+export default Upload
