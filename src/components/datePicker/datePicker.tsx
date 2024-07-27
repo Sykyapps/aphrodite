cddee49f988c73856613fdc810be138ac0a3e0db
Irfan Nurghiffari Muhajir <148177230+irfanatsayakaya@users.noreply.github.com>
2024-07-27 11:32:49 +0700

    fix: update date picker

--- src/components/datePicker/datePicker.tsx
+++ src/components/datePicker/datePicker.tsx
@@ -3,10 +3,37 @@ import { DayPicker, DayPickerSingleProps } from "react-day-picker"
 
 import "./datePicker.scss"
 
-type DatePickerProps = Omit<DayPickerSingleProps, "locale">
+type DatePickerProps = {
+  onSelect: (range: Date | undefined) => void
+} & Omit<
+  DayPickerSingleProps,
+  "locale" | "mode" | "captionLayout" | "disabled" | "onSelect"
+>
 
-const DatePicker = (props: DatePickerProps) => {
-  return <DayPicker locale={id} {...props} />
+const DatePicker = ({ onSelect, ...props }: DatePickerProps) => {
+  const handleOnSelect = (date: Date | undefined) => {
+    if (!date) {
+      onSelect(undefined)
+      return
+    }
+
+    const selectedDate = new Date(date.setHours(7))
+    onSelect(selectedDate)
+  }
+
+  return (
+    <DayPicker
+      mode="single"
+      captionLayout="dropdown"
+      locale={id}
+      disabled={{
+        from: new Date(+new Date() + 86400000),
+        to: new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0),
+      }}
+      onSelect={handleOnSelect}
+      {...props}
+    />
+  )
 }
 
 export type { DatePickerProps }
