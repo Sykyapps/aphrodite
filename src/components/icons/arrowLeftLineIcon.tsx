fc4fbacf93a994e64c07f39e7d49ec301a68196c
Irfan Nurghiffari Muhajir <148177230+irfanatsayakaya@users.noreply.github.com>
2024-08-05 14:37:59 +0700

    Release/0.1.13 (#14)
    * chore: up version 0.1.13
    
    * feat: add autocomplete select
    
    * feat: add arrow line icon
    
    * feat: add downloadable upload

---
+++ src/components/icons/arrowLeftLineIcon.tsx
@@ -0,0 +1,24 @@
+import type { SVGProps } from "react"
+const SvgArrowLeftLineIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <g clipPath="url(#arrowLeftLineIcon_svg__a)">
+      <path
+        fill="#1C1D1F"
+        d="M7.828 11H20v2H7.828l5.364 5.364-1.414 1.414L4 12l7.778-7.778 1.414 1.414z"
+      />
+    </g>
+    <defs>
+      <clipPath id="arrowLeftLineIcon_svg__a">
+        <path fill="#fff" d="M0 0h24v24H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+)
+export default SvgArrowLeftLineIcon
