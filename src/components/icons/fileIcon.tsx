75ddaccaf04dda45337e77c7ae4c8812c5563024
Irfan Nurghiffari Muhajir <148177230+irfanatsayakaya@users.noreply.github.com>
2024-09-09 11:25:55 +0700

    fix: remove color in file icon

--- src/components/icons/fileIcon.tsx
+++ src/components/icons/fileIcon.tsx
@@ -9,7 +9,7 @@ const SvgFileIcon = (props: SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path
-      fill="#1C1D1F"
+      fill="currentColor"
       d="m3 8 6.003-6h10.995C20.55 2 21 2.455 21 2.992v18.016a.993.993 0 0 1-.993.992H3.993A1 1 0 0 1 3 20.993zm7-4.5L4.5 9H10z"
     />
   </svg>
